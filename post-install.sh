fa6e511e79730a30c96ef9d80cb819fc8bbbd0d8
RobRoyEerkes <rreerkes@hotmail.com>
2025-10-11 01:16:35 +0200

    fleshed out the post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 notify-send "hi from curl"
-yay -S --noconfirm --needed zen-browser-bin
+yay -S --noconfirm --needed zen-browser-bin rmpc mpd stow ghostty
+
+# nvim installation
+rm -rf ~/.config/nvim/
+rm -rf ~/.config/uwsm/
+rm -rf ~/.config/ghostty/
+git clone https://github.com/RobRoyEerkes/dotfiles
+cd dotfiles
+stow {nvim, rmpc, ghostty, uwsm}
+xdg-settings set default-web-browser zen.desktop
+
+systemctl --user enable --now mpd
