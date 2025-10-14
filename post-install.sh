3aae413be2dd8f46e4dbec92670c0daa64dfa510
RobRoyEerkes <rreerkes@hotmail.com>
2025-10-14 21:53:09 +0200

    glab

--- post-install.sh
+++ post-install.sh
@@ -14,7 +14,7 @@ if glab auth status &>/dev/null; then
     echo "✅ GitLab CLI already authenticated."
 else
     echo "🔑 Logging into GitHub..."
-    glab auth login --web --hostname gitlab.com
+    glab auth login --hostname gitlab.com
 	
 	git config --global credential.https://gitlab.com.helper '!/usr/bin/glab auth git-credential'
 fi
