b9a653a2e479605d03480673acf9645f97c8d280
RobRoyEerkes <rreerkes@hotmail.com>
2025-10-15 00:08:43 +0200

    correct checkout

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+
+echo "Installing packages"
+yay -Syu --noconfirm --needed zen-browser-bin rmpc mpd stow ghostty udisks2 openrgb glab
+
+xdg-settings set default-web-browser zen.desktop
+
+
 if gh auth status &>/dev/null; then
     echo "✅ GitHub CLI already authenticated."
 else
@@ -7,9 +15,6 @@ else
     gh auth login -p https -w -c
 	gh auth setup-git
 fi
-
-echo "Installing packages"
-yay -Syu --noconfirm --needed zen-browser-bin rmpc mpd stow ghostty udisks2 openrgb glab
 
 
 echo "Installing dotfiles"
@@ -20,11 +25,10 @@ rm -rf ~/.config/ghostty/
 rm -rf ~/.config/hypr/
 rm -rf dotfiles
 git clone -b omarchy https://github.com/RobRoyEerkes/dotfiles
-git checkout omarchy
 cd dotfiles
+git checkout omarchy
 stow -S {nvim,rmpc,ghostty,uwsm,hypr}
 cd
-xdg-settings set default-web-browser zen.desktop
 
 mkdir -p ~/Music/{Playlists,lyrics}
 systemctl --user enable --now mpd
